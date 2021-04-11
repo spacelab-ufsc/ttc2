489dd653d6d4458cb9ed75d2b1f8f5f7adf40b92
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-04-11 03:29:45 -0300

    firmware: drivers: tps382x: Customizing the imported driver#12

--- firmware/drivers/tps382x/tps382x.c
+++ firmware/drivers/tps382x/tps382x.c
@@ -1,22 +1,22 @@
 /*
  * tps382x.c
  * 
- * Copyright (C) 2019, SpaceLab.
+ * Copyright (C) 2021, SpaceLab.
  * 
- * This file is part of OBDH 2.0.
+ * This file is part of TTC 2.0.
  * 
- * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * TTC 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * OBDH 2.0 is distributed in the hope that it will be useful,
+ * TTC 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * along with TTC 2.0. If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 
@@ -25,9 +25,9 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.1.0
+ * \version 0.0.14
  * 
- * \date 15/01/2020
+ * \date 2020/01/15
  * 
  * \addtogroup tps382x
  * \{
@@ -37,7 +37,11 @@
 
 int tps382x_init(tps382x_config_t config)
 {
-    return gpio_init(config.wdi_pin, (gpio_config_t){.mode=GPIO_MODE_OUTPUT});
+    gpio_config_t gpio_conf = {0};
+
+    gpio_conf.mode = GPIO_MODE_OUTPUT;
+
+    return gpio_init(config.wdi_pin, gpio_conf);
 }
 
 void tps382x_trigger(tps382x_config_t config)
