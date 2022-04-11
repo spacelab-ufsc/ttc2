38bcf3a534bc7e085b372fe6ffb6c92a5d394df0
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2022-04-11 10:02:22 -0300

    firmware: libs: containers: Adapting and improving the buffer implementation #64

--- firmware/libs/containers/buffer.c
+++ firmware/libs/containers/buffer.c
@@ -1,33 +1,33 @@
-/* 
+/*
  * buffer.c
  * 
- * Copyright (C) 2017, Universidade Federal de Santa Catarina
+ * Copyright The TTC 2.0 Contributors.
  * 
- * This file is part of FloripaSat-Beacon.
+ * This file is part of TTC 2.0.
  * 
- * FloripaSat-Beacon is free software: you can redistribute it and/or modify
+ * TTC 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * FloripaSat-Beacon is distributed in the hope that it will be useful,
+ * TTC 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with FloripaSat-Beacon. If not, see <http://www.gnu.org/licenses/>.
+ * along with TTC 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
  * 
  */
 
 /**
- * \brief Basic buffer manipulation functions implementation.
+ * \brief Buffer implementation.
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 1.0-dev
+ * \version 0.1.14
  * 
- * \date 09/11/2017
+ * \date 2017/11/09
  * 
  * \addtogroup buffer
  * \{
@@ -35,96 +35,98 @@
 
 #include "buffer.h"
 
-void buffer_init(Buffer *buffer)
+void buffer_init(buffer_t *buffer)
 {
+    buffer->mtu = BUFFER_LENGTH;
+
     buffer_clear(buffer);
 }
 
-uint8_t buffer_length(Buffer *buffer)
+uint16_t buffer_length(buffer_t *buffer)
 {
-    return BUFFER_LENGTH;
+    return buffer->mtu;
 }
 
-bool buffer_fill(Buffer *buffer, uint8_t *data, uint8_t len)
+bool buffer_fill(buffer_t *buffer, uint8_t *data, uint16_t len)
 {
+    bool res = false;
+
     if (len <= buffer_length(buffer))
     {
         buffer_clear(buffer);
-        
-        uint8_t i = 0;
-        for(i=0; i<len; i++)
+
+        uint16_t i = 0;
+        for(i = 0; i < len; i++)
         {
             buffer->data[i] = data[i];
         }
-        
+
         buffer->size = len;
-        
-        return true;
+
+        res = true;
     }
-    else
-    {
-        return false;
-    }
+
+    return res;
 }
 
-bool buffer_append(Buffer *buffer, uint8_t *data, uint8_t len)
+bool buffer_append(buffer_t *buffer, uint8_t *data, uint16_t len)
 {
-    if ((buffer->size + len) <= BUFFER_LENGTH)
+    bool res = false;
+
+    if ((buffer->size + len) <= buffer_length(buffer))
     {
-        uint8_t i = 0;
-        for(i=0; i<len; i++)
+        uint16_t i = 0U;
+        for(i = 0U; i < len; i++)
         {
             buffer->data[buffer->size + i] = data[i];
         }
-        
+
         buffer->size += len;
-        
-        return true;
+
+        res = true;
     }
-    else
-    {
-        return false;
-    }
+
+    return res;
 }
 
-void buffer_clear(Buffer *buffer)
+void buffer_clear(buffer_t *buffer)
 {
-    uint8_t i = 0;
-    for(i=0; i<BUFFER_LENGTH; i++)
+    uint8_t i = 0U;
+    for(i = 0U; i < buffer_length(buffer); i++)
     {
         buffer->data[i] = BUFFER_DEFAULT_BYTE;
     }
     
-    buffer->size = 0;
+    buffer->size = 0U;
 }
 
-bool buffer_empty(Buffer *buffer)
+bool buffer_empty(buffer_t *buffer)
 {
-    if (buffer_size(buffer) == 0)
+    bool res = false;
+
+    if (buffer_size(buffer) == 0U)
     {
-        return true;
+        res = true;
     }
-    else
-    {
-        return false;
-    }
+
+    return res;
 }
 
-bool buffer_full(Buffer *buffer)
+bool buffer_full(buffer_t *buffer)
 {
-    if (buffer_size(buffer) == BUFFER_LENGTH)
+    bool res = false;
+
+    if (buffer_size(buffer) == buffer_length(buffer))
     {
-        return true;
+        res = true;
     }
-    else
-    {
-        return false;
-    }
+
+    return res;
 }
 
-uint8_t buffer_size(Buffer *buffer)
+uint16_t buffer_size(buffer_t *buffer)
 {
     return buffer->size;
 }
 
-//! \} End of buffer group
+/**< \} End of buffer group */
--- firmware/libs/containers/buffer.h
+++ firmware/libs/containers/buffer.h
@@ -1,36 +1,36 @@
-/* 
+/*
  * buffer.h
  * 
- * Copyright (C) 2017, Universidade Federal de Santa Catarina
+ * Copyright The TTC 2.0 Contributors.
  * 
- * This file is part of FloripaSat-Beacon.
+ * This file is part of TTC 2.0.
  * 
- * FloripaSat-Beacon is free software: you can redistribute it and/or modify
+ * TTC 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * FloripaSat-Beacon is distributed in the hope that it will be useful,
+ * TTC 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with FloripaSat-Beacon. If not, see <http://www.gnu.org/licenses/>.
+ * along with TTC 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
  * 
  */
 
 /**
- * \brief Basic buffer.
+ * \brief Buffer definition.
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 1.0-dev
+ * \version 0.1.14
  * 
- * \date 09/11/2017
+ * \date 2017/11/09
  * 
  * \defgroup buffer Buffer
- * \ingroup system
+ * \ingroup containers
  * \{
  */
 
@@ -40,27 +40,28 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define BUFFER_LENGTH           128
+#define BUFFER_LENGTH           300U    /**< Buffer length in bytes. */
 
-#define BUFFER_DEFAULT_BYTE     0xFF
+#define BUFFER_DEFAULT_BYTE     0xFFU   /**< Buffer length in bytes. */
 
 /**
  * \brief Buffer implementation as a struct.
  */
 typedef struct
 {
-    uint8_t data[BUFFER_LENGTH];    /**< Data of the buffer. */
-    uint8_t size;                   /**< Number of elements into the buffer. */
-} Buffer;
+    uint8_t data[BUFFER_LENGTH];        /**< Data of the buffer. */
+    uint16_t size;                      /**< Number of elements into the buffer. */
+    uint16_t mtu;                       /**< Maximum transmission unit. */
+} buffer_t;
 
 /**
  * \brief Buffer initialization.
  * 
- * \param buffer is a pointer to a Buffer struct.
+ * \param[in,out] buffer is a pointer to a Buffer struct.
  * 
  * \return None.
  */
-void buffer_init(Buffer *buffer);
+void buffer_init(buffer_t *buffer);
 
 /**
  * \brief Returns the length (capacity) of a buffer.
@@ -69,66 +70,70 @@ void buffer_init(Buffer *buffer);
  * 
  * \return The length of the buffer (or capacity).
  */
-uint8_t buffer_length(Buffer *buffer);
+uint16_t buffer_length(buffer_t *buffer);
 
 /**
  * \brief Fills the buffer with data.
  * 
- * \param buffer is a pointer to a Buffer struct.
- * \param data is a pointer to an array with the data to store in the buffer.
- * \param len is the length of data.
+ * \param[in,out] buffer is a pointer to a Buffer struct.
+ *
+ * \param[in,out] data is a pointer to an array with the data to store in the buffer.
+ *
+ * \param[in] len is the length of data.
  * 
  * \return True/False if the length of the data fits into the buffer.
  */
-bool buffer_fill(Buffer *buffer, uint8_t *data, uint8_t len);
+bool buffer_fill(buffer_t *buffer, uint8_t *data, uint16_t len);
 
 /**
  * \brief Append data to a buffer.
  * 
- * \param buffer is a pointer to a Buffer struct.
- * \param data is a pointer to an array with the data to append in the buffer.
- * \param len is the length of data.
+ * \param[in,out] buffer is a pointer to a Buffer struct.
+ *
+ * \param[in,out] data is a pointer to an array with the data to append in the buffer.
+ *
+ * \param[in] len is the length of data.
  * 
  * \return True/False if the length of the data fits into the buffer.
  */
-bool buffer_append(Buffer *buffer, uint8_t *data, uint8_t len);
+bool buffer_append(buffer_t *buffer, uint8_t *data, uint16_t len);
 
 /**
  * \brief Clear a buffer filling it with the dafult byte (\b BUFFER_DEFAULT_BYTE).
  * 
- * \param buffer is a pointer to a Buffer struct.
+ * \param[in,out] buffer is a pointer to a Buffer struct.
  * 
  * \return None
  */
-void buffer_clear(Buffer *buffer);
+void buffer_clear(buffer_t *buffer);
 
 /**
  * \brief Verifies if the a buffer is empty or not.
  * 
- * \param buffer is a pointer to a Buffer struct.
+ * \param[in,out] buffer is a pointer to a Buffer struct.
  * 
  * \return True/False if the buffer is empty or not.
  */
-bool buffer_empty(Buffer *buffer);
+bool buffer_empty(buffer_t *buffer);
 
 /**
  * \brief Verifies if the a buffer is full or not.
  * 
- * \param buffer is a pointer to a Buffer struct.
+ * \param[in,out] buffer is a pointer to a Buffer struct.
  * 
  * \return True/False if the buffer is full or not.
  */
-bool buffer_full(Buffer *buffer);
+bool buffer_full(buffer_t *buffer);
 
 /**
  * \brief Returns the size of a buffer.
  * 
- * \param buffer is a pointer to a Buffer struct.
+ * \param[in,out] buffer is a pointer to a Buffer struct.
  * 
  * \return The size of the buffer.
  */
-uint8_t buffer_size(Buffer *buffer);
+uint16_t buffer_size(buffer_t *buffer);
 
-#endif // BUFFER_H_
+#endif /* BUFFER_H_ */
 
-//! \} End of buffer group
+/**< \} End of buffer group */
