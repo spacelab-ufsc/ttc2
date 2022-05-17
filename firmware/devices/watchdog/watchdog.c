7820c602acd7587e2715427da90abcf6b1e3e262
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2022-05-17 18:39:45 -0300

    firmware: devices: watchdog: Fixing MISRA-C 2012 rule

--- firmware/devices/watchdog/watchdog.c
+++ firmware/devices/watchdog/watchdog.c
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with TTC 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * along with TTC 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
  * 
  */
 
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.1.15
+ * \version 0.2.0
  * 
  * \date 2019/11/01
  * 
