ce79b38f788b8fe888ad516c8cc538f7e2b9481c
miguelboing <miguel-boing@hotmail.com>
2022-08-08 00:17:23 -0300

    firmware: tests: devices: Unitary test for power_sensor init function #20

--- firmware/tests/devices/power_sensor_test.c
+++ firmware/tests/devices/power_sensor_test.c
@@ -41,40 +41,144 @@
 #include <float.h>
 #include <cmocka.h>
 
+
 #include <devices/power_sensor/power_sensor.h>
 #include <drivers/ina22x/ina22x.h>
 
+typedef enum
+{
+    INITIALIZATION=0,
+    CONFIGURATION,
+    CALIBRATION
+}config_type_t;
+
+void write_config_test(ina22x_config_t config_test, config_type_t config_type);
+
+ina22x_config_t uc_config = {
+                                    .i2c_port = I2C_PORT_1,
+                                    .i2c_conf.speed_hz = 100000,
+                                    .i2c_adr  = 0x44,
+                                    .avg_mode = INA22X_AVERAGING_MODE_128,
+                                    .bus_voltage_conv_time = INA22X_BUS_VOLTAGE_CONV_TIME_588u,
+                                    .shunt_voltage_conv_time = INA22X_SHUNT_VOLTAGE_CONV_TIME_588u,
+                                    .op_mode = INA22X_MODE_SHUNT_BUS_CONT,
+                                    .lsb_current = 5e-6,
+                                    .cal = 10240,
+} ;
+ina22x_config_t radio_config = {
+                                    .i2c_port = I2C_PORT_1,
+                                    .i2c_conf.speed_hz = 100000,
+                                    .i2c_adr  = 0x45,
+                                    .avg_mode = INA22X_AVERAGING_MODE_128,
+                                    .bus_voltage_conv_time = INA22X_BUS_VOLTAGE_CONV_TIME_588u,
+                                    .shunt_voltage_conv_time = INA22X_SHUNT_VOLTAGE_CONV_TIME_588u,
+                                    .op_mode = INA22X_MODE_SHUNT_BUS_CONT,
+                                    .lsb_current = 5e-5,
+                                    .cal = 1024,
+
+} ;
+
 static void power_sensor_init_test(void **state)
 {
+    /* Initialization */
+    write_config_test(radio_config, INITIALIZATION);
+
+    write_config_test(uc_config, INITIALIZATION);
+
+    /* Configuration */
+    write_config_test(radio_config, CONFIGURATION);
+
+    write_config_test(uc_config, CONFIGURATION);
+
+    /* Calibration */
+    write_config_test(radio_config, CALIBRATION);
+
+    write_config_test(uc_config, CALIBRATION);
+
+ 
+    assert_return_code(power_sensor_init(), 0);
 }
 
 static void power_sensor_read_test(void **state)
 {
 }
 
-static void power_sensor_read_voltage_mv_test(void **state)
+static void power_sensor_read_voltage_scaled_test(void **state)
 {
 }
 
-static void power_sensor_read_current_ma_test(void **state)
+static void power_sensor_read_current_scaled_test(void **state)
 {
 }
 
-static void power_sensor_read_power_mw_test(void **state)
+static void power_sensor_read_power_scaled_test(void **state)
 {
+
 }
+
 
 int main(void)
 {
     const struct CMUnitTest power_sensor_tests[] = {
         cmocka_unit_test(power_sensor_init_test),
         cmocka_unit_test(power_sensor_read_test),
-        cmocka_unit_test(power_sensor_read_voltage_mv_test),
-        cmocka_unit_test(power_sensor_read_current_ma_test),
-        cmocka_unit_test(power_sensor_read_power_mw_test),
+        cmocka_unit_test(power_sensor_read_voltage_scaled_test),
+        cmocka_unit_test(power_sensor_read_current_scaled_test),
+        cmocka_unit_test(power_sensor_read_power_scaled_test),
     };
 
     return cmocka_run_group_tests(power_sensor_tests, NULL, NULL);
+}
+
+void write_config_test(ina22x_config_t config_test, config_type_t config_type)
+{
+    switch(config_type)
+    {
+        case INITIALIZATION:
+            expect_value(__wrap_ina22x_init, config.i2c_port, config_test.i2c_port);
+            expect_value(__wrap_ina22x_init, config.i2c_conf.speed_hz, config_test.i2c_conf.speed_hz);
+            expect_value(__wrap_ina22x_init, config.i2c_adr, config_test.i2c_adr);
+            expect_value(__wrap_ina22x_init, config.avg_mode, config_test.avg_mode);
+            expect_value(__wrap_ina22x_init, config.bus_voltage_conv_time, config_test.bus_voltage_conv_time);
+            expect_value(__wrap_ina22x_init, config.shunt_voltage_conv_time, config_test.shunt_voltage_conv_time);
+            expect_value(__wrap_ina22x_init, config.op_mode, config_test.op_mode);
+            expect_value(__wrap_ina22x_init, config.lsb_current, config_test.lsb_current);
+            expect_value(__wrap_ina22x_init, config.cal, config_test.cal);
+
+            will_return(__wrap_ina22x_init, 0);
+
+            break;
+
+        case CONFIGURATION:
+            expect_value(__wrap_ina22x_configuration, config.i2c_port, config_test.i2c_port);
+            expect_value(__wrap_ina22x_configuration, config.i2c_conf.speed_hz, config_test.i2c_conf.speed_hz);
+            expect_value(__wrap_ina22x_configuration, config.i2c_adr, config_test.i2c_adr);
+            expect_value(__wrap_ina22x_configuration, config.avg_mode, config_test.avg_mode);
+            expect_value(__wrap_ina22x_configuration, config.bus_voltage_conv_time, config_test.bus_voltage_conv_time);
+            expect_value(__wrap_ina22x_configuration, config.shunt_voltage_conv_time, config_test.shunt_voltage_conv_time);
+            expect_value(__wrap_ina22x_configuration, config.op_mode, config_test.op_mode);
+            expect_value(__wrap_ina22x_configuration, config.lsb_current, config_test.lsb_current);
+            expect_value(__wrap_ina22x_configuration, config.cal, config_test.cal);
+
+            will_return(__wrap_ina22x_configuration, 0);
+
+            break;
+
+        case CALIBRATION:
+            expect_value(__wrap_ina22x_calibration, config.i2c_port, config_test.i2c_port);
+            expect_value(__wrap_ina22x_calibration, config.i2c_conf.speed_hz, config_test.i2c_conf.speed_hz);
+            expect_value(__wrap_ina22x_calibration, config.i2c_adr, config_test.i2c_adr);
+            expect_value(__wrap_ina22x_calibration, config.avg_mode, config_test.avg_mode);
+            expect_value(__wrap_ina22x_calibration, config.bus_voltage_conv_time, config_test.bus_voltage_conv_time);
+            expect_value(__wrap_ina22x_calibration, config.shunt_voltage_conv_time, config_test.shunt_voltage_conv_time);
+            expect_value(__wrap_ina22x_calibration, config.op_mode, config_test.op_mode);
+            expect_value(__wrap_ina22x_calibration, config.lsb_current, config_test.lsb_current);
+            expect_value(__wrap_ina22x_calibration, config.cal, config_test.cal);
+
+            will_return(__wrap_ina22x_calibration, 0);
+
+            break;
+    }  
 }
 
 /** \} End of power_sensor_test group */
