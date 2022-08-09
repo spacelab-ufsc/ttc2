d2f0baee38f754a6e50367ba24dcd9876f5031c2
Miguel Boing <miguel-boing@hotmail.com>
2022-08-09 17:27:02 -0300

    firmware: tests: devices: Initial test for sensor reading #20

--- firmware/tests/devices/power_sensor_test.c
+++ firmware/tests/devices/power_sensor_test.c
@@ -49,8 +49,15 @@ typedef enum
 {
     INITIALIZATION=0,
     CONFIGURATION,
-    CALIBRATION
+    CALIBRATION,
+    VOLTAGE
 }config_type_t;
+
+power_sensor_data_t test_data;
+
+float f_volt_i_b, f_volt_i_s;
+float f_curr;
+float f_pwr;
 
 void write_config_test(ina22x_config_t config_test, config_type_t config_type);
 
@@ -101,30 +108,67 @@ static void power_sensor_init_test(void **state)
 
 static void power_sensor_read_test(void **state)
 {
+
+    /* Bus Voltage */
+    write_config_test(uc_config, VOLTAGE);
+
+    expect_value(__wrap_ina22x_get_voltage_V, device, INA22X_BUS_VOLTAGE);
+
+    //expect_memory(__wrap_ina22x_get_voltage_V, volt, &f_volt_i_b, 1); // Primeira coisa a se olhar se teste falhar
+    expect_value(__wrap_ina22x_get_voltage_V, volt, &f_volt_i_b);
+
+    will_return(__wrap_ina22x_get_voltage_V, 0);
+
+    /* Shunt Voltage */
+    write_config_test(uc_config, VOLTAGE);
+
+    expect_value(__wrap_ina22x_get_voltage_V, device, INA22X_SHUNT_VOLTAGE);
+
+    //expect_memory(__wrap_ina22x_get_voltage_V, volt, &f_volt_i_s, 1); // Primeira coisa a se olhar se teste falhar
+    expect_value(__wrap_ina22x_get_voltage_V, volt, &f_volt_i_s);
+
+    will_return(__wrap_ina22x_get_voltage_V, 0);
+
+
+    /* Current */
+    expect_value(__wrap_ina22x_get_current_A, config.i2c_port, uc_config.i2c_port);
+    expect_value(__wrap_ina22x_get_current_A, config.i2c_conf.speed_hz, uc_config.i2c_conf.speed_hz);
+    expect_value(__wrap_ina22x_get_current_A, config.i2c_adr, uc_config.i2c_adr);
+    expect_value(__wrap_ina22x_get_current_A, config.avg_mode, uc_config.avg_mode);
+    expect_value(__wrap_ina22x_get_current_A, config.bus_voltage_conv_time, uc_config.bus_voltage_conv_time);
+    expect_value(__wrap_ina22x_get_current_A, config.shunt_voltage_conv_time, uc_config.shunt_voltage_conv_time);
+    expect_value(__wrap_ina22x_get_current_A, config.op_mode, uc_config.op_mode);
+    expect_value(__wrap_ina22x_get_current_A, config.lsb_current, uc_config.lsb_current);
+    expect_value(__wrap_ina22x_get_current_A, config.cal, uc_config.cal);
+
+    expect_memory(__wrap_ina22x_get_voltage_V, curr, &f_curr, 1); // Primeira coisa a se olhar se teste falhar
+
+    will_return(__wrap_ina22x_get_current_A, 0);
+
+
+    /* Power */
+    expect_value(__wrap_ina22x_get_power_W, config.i2c_port, uc_config.i2c_port);
+    expect_value(__wrap_ina22x_get_power_W, config.i2c_conf.speed_hz, uc_config.i2c_conf.speed_hz);
+    expect_value(__wrap_ina22x_get_power_W, config.i2c_adr, uc_config.i2c_adr);
+    expect_value(__wrap_ina22x_get_power_W, config.avg_mode, uc_config.avg_mode);
+    expect_value(__wrap_ina22x_get_power_W, config.bus_voltage_conv_time, uc_config.bus_voltage_conv_time);
+    expect_value(__wrap_ina22x_get_power_W, config.shunt_voltage_conv_time, uc_config.shunt_voltage_conv_time);
+    expect_value(__wrap_ina22x_get_power_W, config.op_mode, uc_config.op_mode);
+    expect_value(__wrap_ina22x_get_power_W, config.lsb_current, uc_config.lsb_current);
+    expect_value(__wrap_ina22x_get_power_W, config.cal, uc_config.cal);
+
+    expect_memory(__wrap_ina22x_get_voltage_V, pwr, &f_pwr, 1); // Primeira coisa a se olhar se teste falhar
+
+    will_return(__wrap_ina22x_get_power_W, 0);
+
+    assert_return_code(power_sensor_read(POWER_SENSOR_UC, &test_data), 0);
 }
-
-static void power_sensor_read_voltage_scaled_test(void **state)
-{
-}
-
-static void power_sensor_read_current_scaled_test(void **state)
-{
-}
-
-static void power_sensor_read_power_scaled_test(void **state)
-{
-
-}
-
 
 int main(void)
 {
     const struct CMUnitTest power_sensor_tests[] = {
         cmocka_unit_test(power_sensor_init_test),
         cmocka_unit_test(power_sensor_read_test),
-        cmocka_unit_test(power_sensor_read_voltage_scaled_test),
-        cmocka_unit_test(power_sensor_read_current_scaled_test),
-        cmocka_unit_test(power_sensor_read_power_scaled_test),
     };
 
     return cmocka_run_group_tests(power_sensor_tests, NULL, NULL);
@@ -178,6 +222,20 @@ void write_config_test(ina22x_config_t config_test, config_type_t config_type)
             will_return(__wrap_ina22x_calibration, 0);
 
             break;
+
+        case VOLTAGE:
+            expect_value(__wrap_ina22x_get_voltage_V, config.i2c_port, config_test.i2c_port);
+            expect_value(__wrap_ina22x_get_voltage_V, config.i2c_conf.speed_hz, config_test.i2c_conf.speed_hz);
+            expect_value(__wrap_ina22x_get_voltage_V, config.i2c_adr, config_test.i2c_adr);
+            expect_value(__wrap_ina22x_get_voltage_V, config.avg_mode, config_test.avg_mode);
+            expect_value(__wrap_ina22x_get_voltage_V, config.bus_voltage_conv_time, config_test.bus_voltage_conv_time);
+            expect_value(__wrap_ina22x_get_voltage_V, config.shunt_voltage_conv_time, config_test.shunt_voltage_conv_time);
+            expect_value(__wrap_ina22x_get_voltage_V, config.op_mode, config_test.op_mode);
+            expect_value(__wrap_ina22x_get_voltage_V, config.lsb_current, config_test.lsb_current);
+            expect_value(__wrap_ina22x_get_voltage_V, config.cal, config_test.cal);
+
+            break;
+
     }  
 }
 
