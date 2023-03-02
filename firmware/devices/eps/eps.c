60b14eeaec15d68b59ab51570005eee4bb1fa8c4
miguelboing <miguel-boing@hotmail.com>
2023-03-02 15:02:56 -0300

    firmware: devices: eps: Removing unused commands #118

--- firmware/devices/eps/eps.c
+++ firmware/devices/eps/eps.c
@@ -38,8 +38,6 @@
 
 #include "eps.h"
 
-static int eps_read_parameter(uint8_t param, cmdpr_data_t *data);
-
 static const uart_ports_e eps_uart_port = UART_PORT_0;
 
 int eps_init(void)
@@ -68,7 +66,6 @@ int eps_init(void)
     return err;
 }
 
-
 int eps_read_request(eps_request_t *eps_request)
 {
     int err = 0;
@@ -79,25 +76,10 @@ int eps_read_request(eps_request_t *eps_request)
         {
             switch(eps_request->command)
             {
-                case CMDPR_CMD_READ_PARAM:
-                    err = uart_read(eps_uart_port, &(eps_request->parameter), 1);
-                    break;
-                case CMDPR_CMD_WRITE_PARAM:
-                    if (uart_read(eps_uart_port, &(eps_request->parameter), 1) == 0)
-                    {
-                        err = eps_read_parameter(eps_request->parameter, &(eps_request->data));
-                    }
-                    else
-                    {
-                        err = -1;
-                    }
-                    break;
                 case CMDPR_CMD_TRANSMIT_PACKET:
                     eps_request->data.data_packet.len = uart_read_available(eps_uart_port);
                     err = uart_read(eps_uart_port, eps_request->data.data_packet.packet, eps_request->data.data_packet.len);
-                    break;
-                case CMDPR_CMD_READ_FIRST_PACKET:
-                    /* Nothing more to do */
+
                     break;
                 default:
                 #if defined(CONFIG_DRIVERS_DEBUG_ENABLED) && (CONFIG_DRIVERS_DEBUG_ENABLED == 1)
@@ -105,6 +87,7 @@ int eps_read_request(eps_request_t *eps_request)
                     sys_log_new_line();
                 #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
                     err = -1;
+
                     break;
             }
         }
@@ -115,42 +98,6 @@ int eps_read_request(eps_request_t *eps_request)
             sys_log_new_line();
         #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
         }
-    }
-
-    return err;
-}
-
-static int eps_read_parameter(uint8_t param, cmdpr_data_t *data)
-{
-    int err = 0;
-    uint8_t read_buffer[4] = {0};
-    switch(cmdpr_param_size(param))
-    {
-        case 1:
-            err = uart_read(eps_uart_port, &(data->param_8), 1);
-            break;
-        case 2:
-            err = uart_read(eps_uart_port, read_buffer, 2);
-            data->param_16 = ((uint16_t)read_buffer[0]) | ((uint16_t)(read_buffer[1])<<8);
-            break;
-        case 4:
-            err = uart_read(eps_uart_port, read_buffer, 4);
-            data->param_32 = ((uint32_t)read_buffer[0]) | ((uint32_t)(read_buffer[1])<<8) | ((uint32_t)(read_buffer[2])<<16) | ((uint32_t)(read_buffer[3])<<24);
-            break;
-        default:
-            err = -1;
-        #if defined(CONFIG_DRIVERS_DEBUG_ENABLED) && (CONFIG_DRIVERS_DEBUG_ENABLED == 1)
-            sys_log_print_event_from_module(SYS_LOG_ERROR, EPS_MODULE_ERROR, "Error reading EPS parameter: unknown parameter!");
-            sys_log_new_line();
-        #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
-            break;
-    }
-    if (err == -1)
-    {
-    #if defined(CONFIG_DRIVERS_DEBUG_ENABLED) && (CONFIG_DRIVERS_DEBUG_ENABLED == 1)
-        sys_log_print_event_from_module(SYS_LOG_ERROR, EPS_MODULE_ERROR, "Error reading EPS parameter: unable to read parameter!");
-        sys_log_new_line();
-    #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
     }
 
     return err;
