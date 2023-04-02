0d792375df570772d234e49deb7f0a20043f987b
miguelboing <miguel-boing@hotmail.com>
2023-04-02 14:51:05 -0300

    firmware: app: tasks: Downlink Manager implementation #125

---
+++ firmware/app/tasks/downlink_manager.c
@@ -0,0 +1,76 @@
+/*
+ * downlink_manager.c
+ *
+ * Copyright The TTC 2.0 Contributors.
+ *
+ * This file is part of TTC 2.0.
+ *
+ * TTC 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TTC 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TTC 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \brief Downlink Manager task implementation.
+ *
+ * \author Miguel Boing <miguelboing13@gmail.com>
+ *
+ * \version 0.4.1
+ *
+ * \date 2023/03/03
+ *
+ * \addtogroup downlink_manager
+ * \{
+ */
+
+#include <system/sys_log/sys_log.h>
+#include <devices/radio/radio.h>
+#include <structs/ttc_data.h>
+
+#include "downlink_manager.h"
+#include "startup.h"
+
+xTaskHandle xTaskDownlinkManagerHandle;
+
+void vTaskDownlinkManager(void)
+{
+    /* Wait startup task to finish */
+    xEventGroupWaitBits(task_startup_status, TASK_STARTUP_DONE, pdFALSE, pdTRUE, pdMS_TO_TICKS(TASK_DOWNLINK_MANAGER_INIT_TIMEOUT_MS));
+
+    /* Delay before the first cycle */
+    vTaskDelay(pdMS_TO_TICKS(TASK_DOWNLINK_MANAGER_INITIAL_DELAY_MS));
+
+    sys_log_print_event_from_module(SYS_LOG_INFO, TASK_DOWNLINK_MANAGER_NAME, "Initializing the Downlink Manager");
+    sys_log_new_line();
+
+    ttc_data_buf.down_buf.position_to_read = 0;
+    ttc_data_buf.down_buf.position_to_write = 0;
+
+    uint8_t tx_packet[300];
+    uint16_t tx_size = 0;
+
+    while(1)
+     {
+        while(ttc_data_buf.radio.tx_fifo_counter > 0)
+        {
+            downlink_pop_packet(tx_packet, &tx_size);
+
+            /*TODO: Implement codification */
+            radio_send(tx_packet, tx_size);
+        }
+
+     }
+
+}
+
+/** \} End of downlink_manager group */
---
+++ firmware/app/tasks/downlink_manager.h
@@ -0,0 +1,64 @@
+/*
+ * downlink_manager.h
+ *
+ * Copyright The TTC 2.0 Contributors.
+ *
+ * This file is part of TTC 2.0.
+ *
+ * TTC 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TTC 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TTC 2.0. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \brief Downlink manager task definition.
+ *
+ * \author Miguel Boing <miguelboing13@gmail.com>
+ *
+ * \version 0.4.1
+ *
+ * \date 2023/03/03
+ *
+ * \defgroup downlink_manager DOWNLINK_MANAGER
+ * \ingroup tasks
+ * \{
+ */
+
+#ifndef TASK_DOWNLINK_MANAGER_H_
+#define TASK_DOWNLINK_MANAGER_H_
+
+#include <FreeRTOS.h>
+#include <task.h>
+
+#define TASK_DOWNLINK_MANAGER_NAME                "Downlink Manager"  /**< Task name. */
+#define TASK_DOWNLINK_MANAGER_STACK_SIZE          2000                /**< Stack size in bytes. */
+#define TASK_DOWNLINK_MANAGER_PRIORITY            5                   /**< Task priority. */
+#define TASK_DOWNLINK_MANAGER_PERIOD_MS           100                 /**< Task period in milliseconds. */
+#define TASK_DOWNLINK_MANAGER_INITIAL_DELAY_MS    1000                /**< Delay, in milliseconds, before the first execution. */
+#define TASK_DOWNLINK_MANAGER_INIT_TIMEOUT_MS     10000               /**< Wait time to initialize the task in milliseconds. */
+
+/**
+ * \brief Downlink Manager handle.
+ */
+extern xTaskHandle xTaskDownlinkManagerHandle;
+
+/**
+ * \brief Downlink Manager task.
+ *
+ * \return None.
+ */
+void vTaskDownlinkManager(void);
+
+#endif /* TASK_DOWNLIN_MANAGER_H_ */
+
+/** \} End of downlink_manager group */
