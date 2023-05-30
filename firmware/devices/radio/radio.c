cd8af5ea77ce2070acb1ac83f9b0601e7adf1348
miguelboing <miguel-boing@hotmail.com>
2023-05-30 14:34:57 -0300

    firmware: devices: radio: Implementation for radio_available function #125

--- firmware/devices/radio/radio.c
+++ firmware/devices/radio/radio.c
@@ -115,8 +115,7 @@ int radio_recv(uint8_t *data, uint16_t len, uint32_t timeout_ms)
 
 int radio_available(void)
 {
-    /*TODO */
-    return -1;
+    return si446x_gpio_get_pin(SI446X_GPIO_NIRQ);
 }
 
 int radio_sleep(void)
