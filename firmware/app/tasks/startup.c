3df3493f3fc43150c87525ecea2519090445bcec
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2023-06-16 01:09:52 -0300

    app: tasks: Adding an error verification to the NGHam initialization in the startup task #136

--- firmware/app/tasks/startup.c
+++ firmware/app/tasks/startup.c
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.4.3
+ * \version 0.4.4
  * 
  * \date 2019/12/04
  * 
@@ -129,8 +129,10 @@ void vTaskStartup(void)
 #endif /* CONFIG_DEV_RADIO_ENABLED */
 
     /* NGHam initialization */
-    ngham_init_arrays();
-    ngham_init();
+    if (ngham_init() != 0)
+    {
+        error_counter++;
+    }
 
     /* Antenna device initialization */
 #if defined(CONFIG_DEV_ANTENNA_ENABLED) && (CONFIG_DEV_ANTENNA_ENABLED == 1)
