c3a19c850e7cf148e3a18e8c6fe29438f7a0e232
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2023-06-16 00:56:28 -0300

    app: libs: Updating the NGHam library

---
+++ firmware/app/libs/ngham-0.1/include/ngham/config.h
@@ -0,0 +1,44 @@
+/*
+ * config.h
+ * 
+ * Copyright The NGHam Contributors.
+ * 
+ * This file is part of NGHam.
+ * 
+ * NGHam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NGHam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NGHam. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Configuration parameters.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 2023/06/15
+ * 
+ * \defgroup config Configuration
+ * \ingroup ngham
+ * \{
+ */
+
+#ifndef NGHAM_CONFIG_H_
+#define NGHAM_CONFIG_H_
+
+#define NGHAM_FOUR_LEVEL_MODULATION     0
+
+#endif /* NGHAM_CONFIG_H_ */
+
+/**< \} End of config group */
