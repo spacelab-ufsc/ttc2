ab7ad7dc5a3261ef7f39f2b4d19c5f955f4e5b71
Miguel Boing <miguel-boing@hotmail.com>
2024-01-16 16:47:55 -0300

    firmware: devices: radio: Fixing radio_available function description #145

--- firmware/devices/radio/radio.h
+++ firmware/devices/radio/radio.h
@@ -83,7 +83,7 @@ int radio_send(uint8_t *data, uint16_t len);
 int radio_recv(uint8_t *data, uint16_t len, uint32_t timeout_ms);
 
 /**
- * \brief Verifies the number of availables bytes to receive.
+ * \brief Verifies if there are unread received bytes.
  *
  * \return The number of available bytes to receive (-1 on error).
  */
