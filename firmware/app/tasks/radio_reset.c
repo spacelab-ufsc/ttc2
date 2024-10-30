bab6423840ac14948efb1f4553f99bcafc50c4eb
miguelboing <miguel-boing@hotmail.com>
2024-10-30 17:03:10 -0300

    firmware: app: tasks: Radio reset task implementation

--- firmware/app/tasks/radio_reset.c
+++ firmware/app/tasks/radio_reset.c
@@ -33,6 +33,8 @@
  * \{
  */
 
+#include <system/sys_log/sys_log.h>
+
 #include "radio_reset.h"
 
 xTaskHandle xTaskRadioResetHandle;
@@ -45,6 +47,19 @@ void vTaskRadioReset(void)
     while(1)
     {
         TickType_t last_cycle = xTaskGetTickCount();
+
+        sys_log_print_event_from_module(SYS_LOG_INFO, TASK_RADIO_RESET_NAME, "Resetting radio device...");
+        sys_log_new_line();
+
+        if (radio_init() != 0)
+        {
+            sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_RADIO_RESET_NAME, "Failed to reset the radio device.");
+        }
+        else
+        {
+            sys_log_print_event_from_module(SYS_LOG_INFO, TASK_RADIO_RESET_NAME, "Successfully reseted the radio device.");
+        }
+        sys_log_new_line();
 
         vTaskDelayUntil(&last_cycle, pdMS_TO_TICKS(TASK_RADIO_RESET_PERIOD_MS));
     }
