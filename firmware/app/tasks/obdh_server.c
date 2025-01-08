1fd3f001e52d1c2937917b8927c1064211de14f4
Miguel Boing <miguel.boing@sc.senai.br>
2025-01-08 11:35:58 -0300

    firmware: app: tasks: Obdh server adjustments for CRC #166

--- firmware/app/tasks/obdh_server.c
+++ firmware/app/tasks/obdh_server.c
@@ -84,7 +84,7 @@ void vTaskObdhServer(void)
 
                         break;
                     case CMDPR_CMD_WRITE_PARAM:
-                        obdh_write_read_bytes(7);
+                        obdh_write_read_bytes(8U);
 
                         switch(obdh_request.parameter)
                         {
@@ -130,7 +130,7 @@ void vTaskObdhServer(void)
                         }
                         break;
                     case CMDPR_CMD_TRANSMIT_PACKET:
-                        obdh_write_read_bytes(7);
+                        obdh_write_read_bytes(8U);
 
                         downlink_add_packet(obdh_request.data.data_packet.packet, obdh_request.data.data_packet.len);
 
@@ -142,12 +142,12 @@ void vTaskObdhServer(void)
 
                         obdh_send_response(&obdh_response);
 
-                        obdh_write_read_bytes(7U);
+                        obdh_write_read_bytes(8U);
 
                         break;
                     case 0x00:
                         /* Read mode */
-                        obdh_write_read_bytes(7U);
+                        obdh_write_read_bytes(8U);
 
                         break;
                     default:
