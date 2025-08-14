6b3e4ef054a9257075567f35940eb809086288bf
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-08-14 01:14:08 -0300

    firmware: app: tasks: uplink_manager: Remove radio reset from uplink manager #172

--- firmware/app/tasks/uplink_manager.c
+++ firmware/app/tasks/uplink_manager.c
@@ -108,7 +108,6 @@ void vTaskUplinkManager(void)
                 }
                 else
                 {
-                    (void)radio_reset();
                     ttc_data_buf.n_conseq_failed_packets++;
 
                     sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_UPLINK_MANAGER_NAME, "Failed to decode a new packet.");
@@ -120,7 +119,6 @@ void vTaskUplinkManager(void)
             }
             else
             {
-                (void)radio_reset();
                 ttc_data_buf.n_conseq_failed_packets++;
 
                 sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_UPLINK_MANAGER_NAME, "Failed to receive a new packet.");
